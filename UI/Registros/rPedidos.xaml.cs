05783b10a0bf84396657daef001613fae71e8139
Jose Luis Burgos Hernandez <jose_burgos3@ucne.edu.do>
2020-07-05 01:24:55 -0400

    - Programando Pedidos.xaml.cs

--- UI/Registros/rPedidos.xaml.cs
+++ UI/Registros/rPedidos.xaml.cs
@@ -9,17 +9,135 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using rDetallado_PedidosSuplidores.BLL;
+using rDetallado_PedidosSuplidores.Entidades;
+using rPartidas_Juego.BLL;
 
 namespace rDetallado_PedidosSuplidores.UI.Registros
 {
-    /// <summary>
-    /// Interaction logic for rPedidos.xaml
-    /// </summary>
     public partial class rPedidos : Window
     {
+        private Ordenes ordenes = new Ordenes();
         public rPedidos()
         {
             InitializeComponent();
+            this.DataContext = ordenes;
+
+            //——————————————————————————[ VALORES DEL ComboBox Suplidores]——————————————————————————
+            SuplidorIdComboBox.SelectedValuePath = "SuplidorId";
+            SuplidorIdComboBox.DisplayMemberPath = "Nombres";
+            SuplidorIdComboBox.ItemsSource = ProductosBLL.GetList();
+
+            //——————————————————————————[ VALORES DEL ComboBox Productos]——————————————————————————
+            ProductoIdComboBox.SelectedValuePath = "ProductoId";
+            ProductoIdComboBox.DisplayMemberPath = "Descripcion";
+            ProductoIdComboBox.ItemsSource = ProductosBLL.GetList();
+        }
+        //—————————————————————————————————————————————————————[ CARGAR ]—————————————————————————————————————————————————————
+        private void Cargar()
+        {
+            this.DataContext = null;
+            this.DataContext = ordenes;
+        }
+        //—————————————————————————————————————————————————————[ LIMPIAR ]—————————————————————————————————————————————————————
+        private void Limpiar()
+        {
+            this.ordenes = new Ordenes();
+            this.DataContext = ordenes;
+        }
+        //—————————————————————————————————————————————————————[ Validar ]—————————————————————————————————————————————————————
+        private bool Validar()
+        {
+            bool Validado = true;
+            if (PedidoIdTextbox.Text.Length == 0)
+            {
+                Validado = false;
+                MessageBox.Show("Transaccion Errada", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            return Validado;
+        }
+        //—————————————————————————————————————————————————————[ BUSCAR ]—————————————————————————————————————————————————————
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Ordenes encontrado = OrdenesBLL.Buscar(ordenes.OrdenId);
+
+            if (encontrado != null)
+            {
+                ordenes = encontrado;
+                Cargar();
+                MessageBox.Show("Partida Encontrada", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show($"La Id de Partida no fue encontrada.\n\nAsegurese que existe o cree una nueva.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Warning);
+                Limpiar();
+                PedidoIdTextbox.Text = "";
+                PedidoIdTextbox.Focus();
+            }
+        }
+        //—————————————————————————————————————————————————————[ AGREGAR FILA ]—————————————————————————————————————————————————————
+        private void AgregarFilaButton_Click(object sender, RoutedEventArgs e)
+        {
+            var filaDetalle = new OrdenesDetalle
+            {
+                OrdenId = this.ordenes.OrdenId,
+                Cantidad = Convert.ToInt32(CantidadTextBox.Text),
+                Costo = (int)Convert.ToDouble(CostoTextBox.Text.ToString())
+            };
+
+            //Todo: Evitar que se agrege el mismo jugador 2 veces.
+            this.ordenes.Detalle.Add(filaDetalle);
+            Cargar();
+
+            SuplidorIdComboBox.SelectedIndex = -1;
+            ProductoIdComboBox.SelectedIndex = -1;
+            CantidadTextBox.Clear();
+            CostoTextBox.Clear();
+        }
+        //—————————————————————————————————————————————————————[ REMOVER FILA ]—————————————————————————————————————————————————————
+        private void RemoverFilaButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (DetalleDataGrid.Items.Count >= 1 && DetalleDataGrid.SelectedIndex <= DetalleDataGrid.Items.Count - 1)
+            {
+                ordenes.Detalle.RemoveAt(DetalleDataGrid.SelectedIndex);
+                Cargar();
+            }
+        }
+        //—————————————————————————————————————————————————————[ NUEVO ]—————————————————————————————————————————————————————
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+        //—————————————————————————————————————————————————————[ GUARDAR ]—————————————————————————————————————————————————————
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                if (!Validar())
+                    return;
+
+                var paso = OrdenesBLL.Guardar(this.ordenes);
+                if (paso)
+                {
+                    Limpiar();
+                    MessageBox.Show("Transaccion Exitosa", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                    MessageBox.Show("Transaccion Errada", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+        //—————————————————————————————————————————————————————[ ELIMINAR ]—————————————————————————————————————————————————————
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                if (OrdenesBLL.Eliminar(Utilidades.ToInt(PedidoIdTextbox.Text)))
+                {
+                    Limpiar();
+                    MessageBox.Show("Registro Eliminado", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                    MessageBox.Show("No se pudo eliminar", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
     }
 }
