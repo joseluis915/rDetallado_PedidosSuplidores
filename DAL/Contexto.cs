794087f937060a85b8bef1539ab12a292f70f3cb
Jose Luis Burgos Hernandez <jose_burgos3@ucne.edu.do>
2020-07-05 00:40:46 -0400

    - Creando el Contexto (Creando la BD y agregando los valores al OnModelCreating)

---
+++ DAL/Contexto.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using rDetallado_PedidosSuplidores.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rDetallado_PedidosSuplidores.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Ordenes> Ordenes { get; set; }
+        public DbSet<Productos> Productos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= Data\PedidosSuplidores_DB");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Productos>().HasData(new Productos { ProductoId = 1, Costo = 50, Descripcion = "Pan"});
+            modelBuilder.Entity<Productos>().HasData(new Productos { ProductoId = 2, Costo = 179, Descripcion = "Jugo de Naranja" });
+
+            modelBuilder.Entity<Suplidores>().HasData(new Suplidores { SuplidoresId = 1, Nombres = "Yoma" });
+            modelBuilder.Entity<Suplidores>().HasData(new Suplidores { SuplidoresId = 2, Nombres = "Rica" });
+        }
+    }
+}
